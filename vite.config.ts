7a4b1220b246ebfc120f08294c8a6fbd5eb9dd44
DawidPolanski <dpolanski1999@gmail.com>
2025-02-05 00:11:15 +0100

    feat: update project title, Vite plugin, and configuration for improved build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
 });
